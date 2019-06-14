84280472479115c5f929f563ca8ecc46ae5cbf27
kevinrsd <riverosk885@gmail.com>
2019-06-14 10:38:46 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,91 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Jarvis from './Components/ReactJarvis/ReactJarvis';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+const recognition = new SpeechRecognition();
+recognition.continous = true;
+recognition.interimResults = true;
+recognition.lang = 'es-PE';
+
+
+export default class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      listening: false,finalText:""
+    }
+    this.toggleListen = this.toggleListen.bind(this)
+    this.handleListen = this.handleListen.bind(this)
+  }
+
+  toggleListen() {
+    this.setState({
+      listening: !this.state.listening
+    }, this.handleListen)
+  }
+  
+  handleListen(){
+    console.log('listening?', this.state.listening)
+
+    if (this.state.listening) {
+      recognition.start()
+      recognition.onend = () => {
+        console.log("...continue listening...")
+        recognition.start()
+      }
+
+    } else {
+      recognition.stop()
+      recognition.onend = () => {
+        console.log("Stopped listening per click")
+      }
+    }
+
+    recognition.onstart = () => {
+      console.log("Listening!")
+    }
+    let finalTranscript = '';
+    recognition.onresult = event => {
+      let interimTranscript = ''
+
+      for (let i = event.resultIndex; i < event.results.length; i++) {
+        const transcript = event.results[i][0].transcript;
+        if (event.results[i].isFinal) finalTranscript += transcript + ' ';
+        else interimTranscript += transcript;
+      }
+      this.setState({finalText:finalTranscript})
+
+
+      const transcriptArr = finalTranscript.split(' ')
+      const stopCmd = transcriptArr.slice(-3, -1)
+      console.log('stopCmd', stopCmd)
+
+      if (stopCmd[0] === 'stop' && stopCmd[1] === 'listening'){
+        recognition.stop()
+        recognition.onend = () => {
+          console.log('Stopped listening per command')
+          const finalText = transcriptArr.slice(0, -3).join(' ')
+          console.log(finalText);
+        }
+      }
+    }
+
+    recognition.onerror = event => {
+      console.log("Error occurred in recognition: " + event.error)
+      this.toggleListen();
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+     
+        <Jarvis />
+        <div className="text-container"><p>{this.state.finalText}</p> </div>
+        <div className="btn-container"><div onClick={this.toggleListen} className="boton"><p>START</p> </div></div>
+        
     </div>
-  );
+    )
+  }
 }
-
-export default App;
